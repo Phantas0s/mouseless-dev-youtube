a8bafaac611ca4e61060ad2d55af832f799acc5a
matthieu <matthieu.cneude@gmail.com>
2022-01-30 17:43:15 +0100

    New files for new video
    Bash best practice 05 - More unit tests!

---
+++ bash_best_practice/backup_05/backup_test.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# file: backup_test.sh
+# Doc: https://github.com/kward/shunit2
+
+# Run before all tests
+function oneTimeSetUp() {
+    script=./backup.sh
+    dirs=./dir.csv
+}
+
+# Run before each test
+function setUp() {
+    rm -rf ./testdata
+    mkdir -p testdata/{source/a_directory,destination}
+    touch testdata/source/{a_file,a_directory/another_file}
+
+    source=testdata/source
+    destination=testdata/destination
+
+    file=./dir.csv
+    echo "$source,$destination" > "$file"
+}
+
+function testOptionHelp() {
+    assertContains "$($script -h)" help
+}
+
+function testOptionVersion() {
+    assertContains "$($script -v)" "Version 1.0"
+}
+
+function testOptionsDryRun() {
+    assertContains "$($script -d $dirs)" "(DRY RUN)"
+}
+
+function testDestination() {
+    "$($script "$file")"
+    assertTrue "[ $(find "$source" | wc -l) == $(find "$destination" | wc -l) ]"
+}
+
+function testDeleteFile() {
+    "$($script "$file")"
+    rm "$source/a_file"
+    assertTrue "[ $(find "$source" | wc -l) != $(find "$destination" | wc -l) ]"
+    "$($script "$file")"
+    assertTrue "[ $(find "$source" | wc -l) == $(find "$destination" | wc -l) ]"
+}
+
+function testCopyAFile() {
+    "$($script "$file")"
+    assertTrue "[ -f $destination/a_file ]"
+}
+
+
+# Run after each test
+function tearDown() {
+    return 0
+}
+
+source shunit2
